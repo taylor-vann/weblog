6358a58f61bfdd8d498c2e1808ff4fb834bb0442
Taylor Vann <brian.t.vann@gmail.com>
2025-09-12 10:56:07 -0700

    config builds

--- weblog-rs/weblog/src/main.rs
+++ weblog-rs/weblog/src/main.rs
@@ -1,3 +1,11 @@
+/*
+    Server for Weblog
+    - serve prebuilt static webpages
+    - dynamically generate pages from templates
+    - (stretch) add auth
+    - (stretch) add "blog poster"
+*/
+
 #[tokio::main]
 async fn main() -> Result<(), String> {
     Ok(())
